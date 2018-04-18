8118512e191d2fbd590926efad9ddbff83df45f9
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-17 19:55:42 -0500

    code a little

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,32 @@
-var animalArr = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
-var randomWord = animalArr[Math.floor(Math.random() * animalArr.length)];
-var s;
-var count = 0;
+var zodiac = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
+//ask computer choose a random word
+var wins = 0;
+var looses = 0;
+var remainingLetter = [];
+var wrongLetter = [];
+var maxChance = 5;
+var randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
 var answerArr = [];
+var userGuess = [];
 
-function guess() {
+
+function guessWord() {
     for (var i = 0; i < randomWord.length; i++) {
-        answerArr[i] = "_" ;
+        answerArr.push ("_");
     }
+    document.getElementById("current-word").textContent = answerArr; 
 
-s = answerArr.join("");
-document.getElementById("answer").innerHTML = s;
+    for (var j = 0; j < randomWord.length; j++) {
+        if (userGuess == randomWord[j]) {
+            answerArr.push(userGuess);
+        }
+        else {
+            answerArr.push("_");
+        }
+    }
+    document.getElementById("current-word").textContent = answerArr;
 }
+
+
+ 
+guessWord();
