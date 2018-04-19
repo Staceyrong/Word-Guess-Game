1651163462c1c36979c1c5697cef7896616ae854
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-19 16:09:04 -0500

    image and audio need to add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
 var zodiac = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
-//ask computer choose a random word
+var randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
 var wins = 0;
 var looses = 0;
 var maxChance = 10;
-var randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
 var answerArr = [];
 var temp = [];
 
-
+//make page show underscores with random word length
 for (var i = 0; i < randomWord.length; i++) {
     answerArr.push ("_");
 }
@@ -17,10 +16,12 @@ output();
 document.onkeyup = function guessWord() {
     var userGuess = event.key;
     answerArr = [];
+    //guess wrong letter show in the page and if enter same letter several times, the maxchance won't be counted.
     if (temp.indexOf(userGuess) == -1) {
         temp.push(userGuess);
         maxChance --;
     }
+    // push the right letter into the "_", others still show "_"
     for (var j = 0; j < randomWord.length; j++) {
         if (temp.indexOf(randomWord[j]) > -1) {
             answerArr.push(randomWord[j]);
