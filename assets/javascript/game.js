f8e538ec575c5c7610be9d6fe34cb2b33a9349ef
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-19 18:23:31 -0500

    images finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,6 +33,54 @@ document.onkeyup = function guessWord() {
     }
     //make an array into a string, guess right, reset the game
     if (answerArr.join("") == randomWord) {
+        if (randomWord == "rat") {
+            document.getElementById("image").src="https://i.pinimg.com/originals/0a/c5/bd/0ac5bd36c10d334d2b985893285caf14.jpg";
+            document.getElementById("name").textContent = "Rat";
+        }
+        if (randomWord == "ox") {
+            document.getElementById("image").src="https://images.clipartof.com/small/1200268-Black-And-White-Cute-Loving-Ox-Calf-Poster-Art-Print.jpg";
+            document.getElementById("name").textContent = "Ox";
+        }
+        if (randomWord == "tiger") {
+            document.getElementById("image").src="http://cdn.emgn.com/wp-content/uploads/2014/06/White-Tiger-Cubs-2.jpg";
+            document.getElementById("name").textContent = "Tiger";
+        }
+        if (randomWord == "rabbit") {
+            document.getElementById("image").src="https://www.suggestedpost.eu/wp-content/uploads/2017/10/cute-bunny-photos12.jpg";
+            document.getElementById("name").textContent = "Rabbit";
+        }
+        if (randomWord == "dragon") {
+            document.getElementById("image").src="https://cdn.vectorstock.com/i/1000x1000/17/70/cute-dragon-vector-2991770.jpg";
+            document.getElementById("name").textContent = "Dragon";
+        }
+        if (randomWord == "snake") {
+            document.getElementById("image").src="https://cdn.vectorstock.com/i/1000x1000/67/80/cute-snake-cartoon-vector-986780.jpg";
+            document.getElementById("name").textContent = "Snake";
+        }
+        if (randomWord == "horse") {
+            document.getElementById("image").src="https://i.pinimg.com/736x/8c/87/f1/8c87f181412dd85b618ffa21da105ed3.jpg";
+            document.getElementById("name").textContent = "Horse";
+        }
+        if (randomWord == "goat") {
+            document.getElementById("image").src="https://sharesloth.com/wp-content/uploads/2014/07/smiling-baby-goat.jpg";
+            document.getElementById("name").textContent = "Goat";
+        }
+        if (randomWord == "monkey") {
+            document.getElementById("image").src="https://s-media-cache-ak0.pinimg.com/originals/8b/28/19/8b281949da552e77305c4dd3c919294c.jpg";
+            document.getElementById("name").textContent = "Monkey";
+        }
+        if (randomWord == "rooster") {
+            document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbQ3LP--FiZEkvPNA0K1SiBdyyJBFgQz_FyXF8DDaBErKwwUit";
+            document.getElementById("name").textContent = "Rooster";
+        }
+        if (randomWord == "dog") {
+            document.getElementById("image").src="http://cdn2-www.dogtime.com/assets/uploads/2011/03/cute-dog-names.jpg";
+            document.getElementById("name").textContent = "Dog";
+        }
+        if (randomWord == "pig") {
+            document.getElementById("image").src="https://i.pinimg.com/originals/5d/cf/ba/5dcfbad7f40cb5841dfbc7c07334e15d.jpg";
+            document.getElementById("name").textContent = "Pig";
+        }
         wins ++;
         randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
         maxChance = 10;
@@ -40,7 +88,7 @@ document.onkeyup = function guessWord() {
     }
 
     output();
-//guess wrong, reset the game
+    //guess wrong, reset the game
     if (maxChance == 0) {
         looses++;
         randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
@@ -57,10 +105,10 @@ function output() {
     document.getElementById("letter").textContent = temp;
 }
 
-function replacingImage(){
-    if (randomWord == "rabbit") {
-    document.getElementById("image").src="https://www.suggestedpost.eu/wp-content/uploads/2017/10/cute-bunny-photos12.jpg"
-    }
-}
+//function replacingImage(){
+   // if (randomWord == "rabbit") {
+    //document.getElementById("image").src="https://www.suggestedpost.eu/wp-content/uploads/2017/10/cute-bunny-photos12.jpg"
+    //}
+//}
 
-replacingImage();
+//replacingImage();
