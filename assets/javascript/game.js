bca16ce048d851c1e80ad25041b09b28757862ea
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-19 14:19:42 -0500

    code more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,33 +2,49 @@ var zodiac = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat"
 //ask computer choose a random word
 var wins = 0;
 var looses = 0;
-var maxChance = 5;
+var maxChance = 10;
 var randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
 var remainingLetter = randomWord.length;
 var answerArr = [];
 var temp = [];
 //var userGuess = [];
 
-//for (var i = 0; i < randomWord.length; i++) {
-   // answerArr.push ("_");
-//}
-//document.getElementById("current-word").textContent = answerArr; 
+for (var i = 0; i < randomWord.length; i++) {
+    answerArr.push ("_");
+}
+output();
 
 
 document.onkeyup = function guessWord() {
     var userGuess = event.key;
-    temp.push(userGuess);
     answerArr = [];
+    if (temp.indexOf(userGuess) == -1) {
+        temp.push(userGuess);
+        maxChance --;
+    }
     for (var j = 0; j < randomWord.length; j++) {
         if (temp.indexOf(randomWord[j]) > -1) {
             answerArr.push(randomWord[j]);
-            remainingLetter--;
+            remainingLetter --;
         }
         else {
             answerArr.push("_");
         }
     }
+    if (answerArr.join("") == randomWord) {
+        wins ++;
+        randomWord  = zodiac[Math.floor(Math.random() * zodiac.length)];
+        maxChance = 10;
+        temp = [];
+    }
+    output()
+}
+
+function output() {
+    document.getElementById("win").textContent = wins;
+    document.getElementById("remaining").textContent = maxChance;
     document.getElementById("current-word").textContent = answerArr;
+    document.getElementById("letter").textContent = temp;
 }
 
 
